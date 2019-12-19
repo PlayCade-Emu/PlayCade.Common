4bf801c3e911969f68cf45e4ce6d543d36218888
Glenn Hevey <glenn.hevey@gmail.com>
2019-12-20 05:56:34 +1100

    Added MetaData information

--- PlayCade.Common/Interfaces/Core.cs
+++ PlayCade.Common/Interfaces/Core.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+using PlayCade.Common.Models;
+
 namespace PlayCade.Common.Interfaces
 {
     public interface Core
     {
         void Run(string rom);
+        MetaData MetaData();
     }
 }
---
+++ PlayCade.Common/Models/MetaData.cs
@@ -0,0 +1,11 @@
+namespace PlayCade.Common.Models
+{
+    public class MetaData
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PlatformName { get; set; }
+        public string PlatformDescription { get; set; }
+        public int MaximumNoPlayers { get; set; }
+    }
+}
