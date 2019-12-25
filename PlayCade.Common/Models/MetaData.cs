6843871d746cd7b4a18458215c05a289b1f9ba7f
Glenn Hevey <glenn.hevey@gmail.com>
2019-12-25 21:24:32 +1100

    Update MetaData.cs
    Removed MaximumNoPlayers

--- PlayCade.Common/Models/MetaData.cs
+++ PlayCade.Common/Models/MetaData.cs
@@ -6,6 +6,5 @@ namespace PlayCade.Common.Models
         public string Description { get; set; }
         public string PlatformName { get; set; }
         public string PlatformDescription { get; set; }
-        public int MaximumNoPlayers { get; set; }
     }
 }
