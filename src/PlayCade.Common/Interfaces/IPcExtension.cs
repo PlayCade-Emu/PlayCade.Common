fe7599986419db537c63c09617842fb167d8de16
Glenn Hevey <glenn.hevey@gmail.com>
2020-06-08 10:55:09 +1000

    Updated to new format
    Updated structure
    removed redundant files
    added new PcExtension interface

---
+++ src/PlayCade.Common/Interfaces/IPcExtension.cs
@@ -0,0 +1,45 @@
+namespace PlayCade.Core.Interfaces
+{
+    public interface IPcExtension
+    {
+        /// <summary>
+        /// Name of the extension
+        /// </summary>
+        string Name { get; set; }
+        
+        /// <summary>
+        /// Description of the extension's functionality
+        /// </summary>
+        string Description { get; set; }
+        
+        /// <summary>
+        /// The platform that the extension has been developed to emulate
+        /// </summary>
+        string Platform { get; set; }
+        
+        /// <summary>
+        /// The description about the platform that the extension has been developed to emulate
+        /// </summary>
+        string PlatformDescription { get; set; }
+
+        /// <summary>
+        /// OnStart is called when the extension is initially loaded.
+        /// </summary>
+        void OnStart();
+        
+        /// <summary>
+        /// OnRun is called when the extension starts running it's content
+        /// </summary>
+        void OnRun(string contentLocation);
+
+        /// <summary>
+        /// OnStop is called when the extension stops running it's content
+        /// </summary>
+        void OnStop();
+        
+        /// <summary>
+        /// OnClose is called when the extension is closing
+        /// </summary>
+        void OnClose();
+    }
+}
